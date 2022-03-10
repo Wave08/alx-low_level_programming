d2a92d8af4545e63a8d6d17a716352a07d021b26
Wave08 <mararakudakwashe@gmail.com>
2022-03-10 14:36:19 -0800

    task4 commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
+ *
  * Return: Always 0 (Success)
  * /
  int main(void)
  {
          puts("\"Programming is like a bulding a multilingualpuzzle");
 	 return (0);
+ }
