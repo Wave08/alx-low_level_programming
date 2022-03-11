f927bb4033f201ae9fd08b5cce6ca7de6d5d3a12
Wave08 <mararakudakwashe@gmail.com>
2022-03-10 16:11:36 -0800

    task6 commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+include < stdio.h >
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char character;
+	int ainteger;
+	long along;
+	long long alonglong;
+	float afloat;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
+	printf("Size of an int: %lu bytes(s)\n", sizeof(ainteger));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
+	printf("Size of a long longa int: %lu byte(s)\n", sizeof(alonglong));
+	printf("SIze of a float: %lu byte(S)\n", sizeof(afloat));
+
+	return (0);
+}
+
