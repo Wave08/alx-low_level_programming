f9980b0eb3dec3b573c96e9d8f5b20e50ca5e9c2
Wave08 <mararakudakwashe@gmail.com>
2022-03-12 03:18:10 -0800

    task2 commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main- print if the number is positive, zero, or negative
+ *
+ * Description: using the mai function
+ * this program prints "Prints the alphabets in lower case
+ * Return 0
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
+
