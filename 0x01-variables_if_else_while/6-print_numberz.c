46d266b4dd9dbe791c742cce40194b129681b55e
Wave08 <mararakudakwashe@gmail.com>
2022-03-12 06:12:00 -0800

    task6 commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print if number is positive, zero, or negative
+ *
+ * Description: using main function
+ * this program prints "Prints all single digit numbers starting from 0
+ * Return: 0
+ */
+int main(void)
+
+{
+	int i;
+
+	for (i = 48 ; i < 58 ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
