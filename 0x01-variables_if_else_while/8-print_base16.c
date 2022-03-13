81c44226c5dd54827900be6099b4f7cf6690804c
Wave08 <mararakudakwashe@gmail.com>
2022-03-13 13:17:12 -0700

    task8 commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -  print if the number is positive, zero, or negative
+ * Description: using the main function
+ * this program prints "Prints all the numbers of base 16 in lowercase
+ * Return: 0
+ */
+int main(void)
+
+{
+	char c;
+
+	for (c = '0' ; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
