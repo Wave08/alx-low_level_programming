c4aa49a36505746827f4cad69b4f2b52eef118cb
Wave08 <mararakudakwashe@gmail.com>
2022-03-18 10:05:32 -0700

    task0 commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,8 @@
 #ifdef MAIN_H
-#define MAIN_H
+#define "MAIN_H"
 
-#include <stdio.h>
-
+#include "stdio.h"
+void print_numbers_(void);
+int _putchar(char c);
 void positive_or_negative(int i);
-
-#endif /* MAIN_H */
+#endif /*MAIN_H*/
