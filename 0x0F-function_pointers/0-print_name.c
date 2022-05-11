d64e45b161776016b17463ab87bba900b38f49a8
Wave08 <mararakudakwashe@gmail.com>
2022-05-11 00:30:30 -0700

    function tha prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "stdlib.h"
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - prints a name
+ * @name: pointer to char
+ * @f: pointer to the function tha returns nothing
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+
+{
+	if (name != NULL && *name != '\0' && f != NULL)
+		f(name);
+}
