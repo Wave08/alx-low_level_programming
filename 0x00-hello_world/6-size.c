dd12078b71ba08a2638c0f45183e20988b43100e
root <root@Kudakwashe>
2024-07-03 21:37:17 +0200

    Finally ticked all the checkers

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,11 +16,11 @@ int main(void)
 	long long int lli;
 	float f;
 
-	printf("Size of a char: %ld byte(s)\n", (unsigned)sizeof(c));
-	printf("Size of an int: %ld bytes(s)\n", (unsigned)sizeof(i));
-	printf("Size of a long int: %ld byte(s)\n", (unsigned)sizeof(li));
-	printf("Size of a long long int: %ld byte(s)\n", (unsigned)sizeof(lli));
-	printf("SIze of a float: %ld byte(s)\n", (unsigned)sizeof(f));
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("SIze of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
 
