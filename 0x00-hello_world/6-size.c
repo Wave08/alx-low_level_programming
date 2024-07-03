7a84684dc0fdb958fd8ae2dcca14eb74a2beeae1
root <root@Kudakwashe>
2024-07-03 21:23:41 +0200

    Code is now properly working

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  * main - print string in the put function
  *
  * Description: using mai function
- * this program prints "Programming is like building a multilingual puzzle
+ * this program prints programming is like building a multilingual puzzle
  * Return: 0
  */
 
@@ -12,15 +12,15 @@ int main(void)
 {
 	char c;
 	int i;
-	long li;
-	long - long lli;
+	long int li;
+	long long int lli;
 	float f;
 
-	printf("Size of a char: %ld byte(s)\n", sizeof(c));
-	printf("Size of an int: %ld bytes(s)\n", sizeof(i));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-	printf("Size of a long - long int: %ld byte(s)\n", sizeof(lli));
-	printf("SIze of a float: %ld byte(s)\n", sizeof(f));
+	printf("Size of a char: %ld byte(s)\n", (unsigned)sizeof(c));
+	printf("Size of an int: %ld bytes(s)\n", (unsigned)sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", (unsigned)sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", (unsigned)sizeof(lli));
+	printf("SIze of a float: %ld byte(s)\n", (unsigned)sizeof(f));
 	return (0);
 }
 
